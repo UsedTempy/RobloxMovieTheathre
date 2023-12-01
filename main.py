94e6e282ff5091ef49b79baf3e55fdfb04379916
Tempy <UsedTemplate@gmail.com>
2023-12-01 15:23:31 +0100

    Added the ability see a all movies that are available to the system and should make it so the client can communicate and receive all the movies.

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-import os
-from flask import *  # Flask, jsonify, request
 from PIL import Image
-from io import BytesIO
+from fastapi import FastAPI, HTTPException, Query
 from moviepy.editor import VideoFileClip
 import base64
-from multiprocessing import Pool, cpu_count
+from multiprocessing import Pool
 
-# Creates a new local API
-app = Flask(__name__)
+app = FastAPI()
 
-# Set the desired frame rate (fps)
 fps = 30
 
 MovieList = {
@@ -22,92 +18,73 @@ def get_frame_data(frame):
     img = img.convert("RGB")
     return img.tobytes().hex()
 
-@app.route('/video', methods=['GET'])
-def get_video():
-    # Get the Movie Name, Buffer Length, and Start Point from the URL parameters
-    movie_name = request.args.get('movie_name')
-    buffer_length = int(request.args.get('buffer_length', default=800))
-    start_point = int(request.args.get('start_point', default=0))
+@app.get('/video/')
+async def get_video(
+    movie_name: str = Query(..., description="Name of the movie"),
+    buffer_length: int = Query(10, description="Buffer length in frames (default: 10)"),
+    start_point: int = Query(0, description="Start point in frames (default: 0)"),
+):
+    print(movie_name, buffer_length, start_point)
 
-    # Check if the movie name exists in the MovieList dictionary
     if movie_name not in MovieList:
-        return jsonify({'error': 'Invalid movie name'})
+        raise HTTPException(status_code=404, detail="Movie not found")
 
-    # Get the file path of the movie
     movie_path = MovieList[movie_name]
 
-    # Open Video Clip
     clip = VideoFileClip(movie_path)
-
-    # Calculate the total number of frames in the video
+    
     total_frames = int(clip.fps * clip.duration)
-
-    # Adjust the buffer length if it exceeds the remaining frames
     buffer_length = min(buffer_length, total_frames - start_point)
-
-    # Set the end point for the buffer
     end_point = start_point + buffer_length
 
-    # Create a list to store the bitmap data for each table
     tables = []
     bitmap_data_list = []
 
     with Pool() as pool:
         for i, frame_data in enumerate(pool.imap_unordered(get_frame_data, clip.iter_frames(fps=fps), chunksize=1), start=1):
-            # Skip frames until the start point is reached
             if i < start_point:
                 continue
 
-            # Append the encoded bitmap data to the list
             bitmap_data_list.append(frame_data)
 
-            # If the desired buffer length is reached, add the current table to the list of tables
             if len(bitmap_data_list) == buffer_length:
                 tables.append(bitmap_data_list)
                 bitmap_data_list = []
-                break  # No need to process remaining frames
+                break
 
-    # If there are any frames remaining, add them to a new table
     if bitmap_data_list:
         tables.append(bitmap_data_list)
 
-    # Convert each table to a list of Base64-encoded strings
     tables = [[base64.b64encode(bytes.fromhex(frame_data)).decode('utf-8') for frame_data in table] for table in tables]
 
-    # Send the tables in a JSON response
     responseData = {
         "map": tables,
         "frame_count": buffer_length
     }
 
-    jsonifiedTable = jsonify(responseData)
-    return jsonifiedTable
+    return responseData
 
-@app.route('/movies', methods=['GET'])
+@app.get('/movies/')
 def get_movies():
     formattedList = {}
     for movie_name in MovieList:
-        img = Image.open("Thumbnails/"+movie_name)
+        img = Image.open("Thumbnails/" + movie_name)
         img = img.resize((720, 480))
         img = img.convert("RGB")
         bitmap_data = img.tobytes().hex()
 
-        formattedList[movie_name]: bitmap_data
+        formattedList[movie_name] = bitmap_data
 
-    return jsonify({'movie_list': formattedList})
+    return {'movie_list': formattedList}
 
-@app.route('/frame_count/', methods=['GET'])
-def get_frame_count():
-    movie_name = request.args.get('movie_name')
-
+@app.get('/frame_count/')
+def get_frame_count(movie_name: str = Query(..., description="Name of the movie")):
+    # Check if the movie name exists in the MovieList dictionary
     if movie_name not in MovieList:
-        return jsonify({'error': 'Invalid movie name'})
+        raise HTTPException(status_code=404, detail="Movie not found")
 
     movie_path = MovieList[movie_name]
     clip = VideoFileClip(movie_path)
     frame_count = int(clip.fps * clip.duration)
 
-    return jsonify({'frame_count': frame_count})
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    return {'frame_count': frame_count}
